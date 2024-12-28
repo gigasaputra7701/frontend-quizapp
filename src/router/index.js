66de495223a56e52a9f538c9f3dc4f19bb74afb1
Muhammad Muqsith Giga Saputra <123441826+gigasaputra7701@users.noreply.github.com>
2024-12-29 01:57:26 +0700

    add routes page

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,39 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../views/Home.vue";
+import Test from "../views/Test.vue";
+import Result from "../views/Result.vue";
+import Login from "../views/Login.vue";
+import Register from "../views/Register.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView,
+      path: "/",
+      name: "home",
+      component: Home,
+    },
+    {
+      path: "/test",
+      name: "test",
+      component: Test,
+    },
+    {
+      path: "/result",
+      name: "result",
+      component: Result,
+    },
+    {
+      path: "/login",
+      name: "login",
+      component: Login,
+    },
+    {
+      path: "/register",
+      name: "register",
+      component: Register,
     },
   ],
-})
+});
 
-export default router
+export default router;
