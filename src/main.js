18df8fdc22d55513a8722bbc0a7f873f3d278c9b
Muhammad Muqsith Giga Saputra <123441826+gigasaputra7701@users.noreply.github.com>
2024-12-30 20:47:44 +0700

    add pinia

--- src/main.js
+++ src/main.js
@@ -3,12 +3,15 @@ import "./assets/main.css";
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import { createPinia } from "pinia";
 
 import Toast from "vue-toastification";
 import "vue-toastification/dist/index.css";
 
 const app = createApp(App);
+const pinia = createPinia();
 
+app.use(pinia);
 app.use(router);
 app.use(Toast);
 app.mount("#app");
